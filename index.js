cf9c6a3f4b393d005a41169b64195c297da56c1a
thereyner <>
2021-06-05 21:39:27 +0000

    JS from JavaScript

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-let language = 'JavaScript';
-
+let language1 = 'Java';
+let language2 = "Script";
 //a) Use string concatenation and two .slice() methods to print 'JS' from 'JavaScript'.
-
+console.log((language1.slice(0,1)).concat(language2.slice(0,1)));
 
 
 //b) Without using .slice(), use method chaining to accomplish the same thing.
